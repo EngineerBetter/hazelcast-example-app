9e8e9d68412706075ec0217a80181330b56a1213
Daniel Jones <daniel.jones@engineerbetter.com>
2016-08-07 14:50:40 +0100

    package as executable fat jar

--- src/main/java/com/engineerbetter/hazelcast/HazelcastExampleApplication.java
+++ src/main/java/com/engineerbetter/hazelcast/HazelcastExampleApplication.java
@@ -1,9 +1,13 @@
 package com.engineerbetter.hazelcast;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HazelcastExampleApplication
 {
-
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HazelcastExampleApplication.class, args);
+	}
 }
